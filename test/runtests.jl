8be2654be8bb85847216a7ff392100febfb8f16b
EdJeeOnGitHub <edjee96@gmail.com>
2021-10-17 16:02:43 -0500

    added simple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,21 @@
 using Jeeves
 using Test
+
+
+rtol = 0.0001
+@testset "OLS perfect fit" begin
+    X = rand(1_000, 5)
+    β = [ 1; 2; 3; 4; 5]
+    y = X*β
+    
+    
+    my_model = Jeeves.OLSModel(y, X, vcov = Jeeves.vcov(:iid))
+    
+    fitted_model = fit(my_model)
+    model_coefs = coef(fitted_model)
+    @test isapprox(model_coefs[1], β[1], rtol = rtol)
+    @test isapprox(model_coefs[2], β[2], rtol = rtol)
+    @test isapprox(model_coefs[3], β[3], rtol = rtol)
+    @test isapprox(model_coefs[4], β[4], rtol = rtol)
+    @test isapprox(model_coefs[5], β[5], rtol = rtol)
+end
