0fdb55cb2d494cf9418970193829555506614e89
EdJeeOnGitHub <edjee96@gmail.com>
2021-10-18 13:43:51 -0500

    some clustering

--- src/vcovCluster.jl
+++ src/vcovCluster.jl
@@ -11,22 +11,6 @@ end
 # Do this
 # http://cameron.econ.ucdavis.edu/research/CGM_twoway_ALL_13may2008.pdf
 
-cluster_var_1 = repeat(["Kentucky", "Mass", "Illinois", "Florida"], 100) 
-cluster_var_2 = repeat(["t_1", "t_2", "t_3", "t_4", "t_5"], 80)
-cluster_var_3 = repeat(["village_1", "village_2"], 200)
-
-using Random
-cluster_matrix = Matrix(hcat(shuffle(cluster_var_1), shuffle(cluster_var_2), shuffle(cluster_var_3)))
-cluster_matrix
-
-X = randn(400, 5)
-ϵ = randn(400, 1)
-β = [1, 2, 3, 4, 5]
-y = X * β    + ϵ
-
-unique(cluster_matrix)
-
-
 function findclusterid(cluster_var)
     cluster_id_dict = Dict(unique(cluster_var) .=> 1:length(unique(cluster_var)))
     cluster_id = getindex.(Ref(cluster_id_dict), cluster_var)
@@ -35,40 +19,19 @@ end
 
 
 
-ones(, 5)
-
-unique(permutations([1, 1, 1]))
-unique(permutations([0, 1, 1]))
-unique(permutations([0, 0, 1]))
-
-
-for a in 1:size_R
-    println(a)
-end
-
-using LinearAlgebra
-
 function createRset(cluster_obj::vcovCluster)
     n_cluster_vars = cluster_obj.n_cluster_vars
-    n_cluster_vars = 3
-    size_R = 2^n_cluster_vars - 1
     ones_matrix = ones(n_cluster_vars, n_cluster_vars)
     triangular_matrix = LinearAlgebra.UpperTriangular(ones_matrix)
     # it's vectors all the way down
     perm_matrix = Vector{Vector{Vector{Int}}}(undef, n_cluster_vars)
-    R_set = Vector{Vector{Float64}}(undef, size_R)
     for i in 1:n_cluster_vars
         perm_matrix[i] = unique(permutations(triangular_matrix[i, :]))
     end
-    perm_matrix
-
     # unnest vec of vec
-
-    getindex.(perm_matrix, 1)
-    reduce(, perm_matrix)
-    collect.(unique(permutations.(triangular_matrix)))
+    R_set = reduce(vcat, perm_matrix)
+    return R_set
 end
-
 
 
 
