358545e650a0d493cc182fa4bcb5eeea629fb5b9
EdJeeOnGitHub <edjee96@gmail.com>
2021-10-21 21:23:49 -0500

    added tidy

--- src/utils.jl
+++ src/utils.jl
@@ -12,3 +12,21 @@ function dummy_matrix(df::DataFrame, column)
     dummy_df = DataFrame(Matrix(permutedims(dummy_m)), convert(Vector{String}, string.(unique(colnames[:]))))
     return dummy_df
 end
+
+"""
+    tidy(fit::Jeeves.FittedOLSModel)
+Inspired by R's broom::tidy. Returns a dataframe where each row is a variable
+and its associated estimate.
+"""
+function tidy(fit::Jeeves.FittedOLSModel)
+    X_names = fit.X_names
+    β = fit.modelfit.β
+    se_β = fit.modelfit.se_β
+    df = DataFrame(
+        term = X_names,
+        β = β,
+        se_β = se_β,
+        t_stat = β ./ se_β
+    )
+    return df
+end
