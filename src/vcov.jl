0dd1158d74da4c83ed57e06b6cfbc0db608d2440
EdJeeOnGitHub <edjee96@gmail.com>
2021-10-23 11:09:49 -0500

    inference on IID vcov now uses simpler method dispatch

--- src/vcov.jl
+++ src/vcov.jl
@@ -4,15 +4,6 @@ struct vcovIID <: vcov
 end
 
 
-function inference(fit::Fit, vcov::vcovIID)
-    R = fit.R
-    σ_sq = fit.modelfit.σ_sq
-    vcov_matrix = inv(cholesky(R' * R)) * σ_sq
-    se = sqrt.(diag(vcov_matrix)) 
-    pval = 2 .* cdf.(TDist(fit.N - fit.K), -abs.(fit.modelfit.β ./ se))   
-    return se, pval, σ_sq, vcov_matrix
-end
-
 function inference(resid::Vector, β::Vector, fit::Model, vcov::vcovIID)
     R = fit.R
     N = fit.N
@@ -23,4 +14,11 @@ function inference(resid::Vector, β::Vector, fit::Model, vcov::vcovIID)
     se = sqrt.(diag(vcov_matrix)) 
     pval = 2 .* cdf.(TDist(fit.N - fit.K), -abs.(β ./ se))   
     return se, pval, σ_sq, vcov_matrix
+end
+
+# Method dispatch on fitted model.
+function inference(fit::Fit, vcov::vcovIID)
+    resid = fit.modelfit.resid
+    β = fit.modelfit.β
+    return return inference(resid, β, fit, vcov) 
 end
